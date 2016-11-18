e214fb1808b47cb91f424ddebbfb07f8d9b207b3
isho <idoshoshani@gmail.com>
2016-11-17 20:25:42 -0800

    objectified sockets

---
+++ ChatPacket.java
@@ -0,0 +1,15 @@
+
+import java.io.*;
+
+public class ChatPacket implements Serializable{
+  String packetType;
+  String packetMessage;
+  public ChatPacket(String type, String message){
+    packetType=type;
+    packetMessage=message;
+  }
+  public ChatPacket(String type){
+    packetType=type;
+    packetMessage=null;
+  }
+}
