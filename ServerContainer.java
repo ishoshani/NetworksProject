2ad20659ded9cad3990f6154e0357271a25023a4
isho <idoshoshani@gmail.com>
2016-11-17 18:59:44 -0800

    made client thread

--- ServerContainer.java
+++ ServerContainer.java
@@ -10,18 +10,17 @@ public class ServerContainer{
     int portNumber = Integer.parseInt(args[0]);
     startServer(portNumber);
   }
+
   public static void startServer(int portNumber){
+    boolean listening = true;
     try(
       ServerSocket serverSocket = new ServerSocket(portNumber);
-      Socket clientSocket = serverSocket.accept();
-      PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
-      ){
-        String inputLine;
-        while((inputLine = in.readLine()) != null){
-          out.println(inputLine);
-        }
-      }catch (IOException e) {
+        ){
+          while(listening){
+            new Connector(serverSocket.accept()).start();
+
+          }
+        }catch (IOException e) {
             System.out.println("Exception caught when trying to listen on port "
                 + portNumber + " or listening for a connection");
             System.out.println(e.getMessage());
